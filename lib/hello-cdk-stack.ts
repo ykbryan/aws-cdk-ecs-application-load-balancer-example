ea0064fe287ffc32856732779af31a6f6083e00c
Bryan Chua <me@bryanchua.com>
2019-10-28 14:10:37 -0700

    cdk for ecs fargate

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,18 +1,31 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2');
+import ecs = require('@aws-cdk/aws-ecs');
+import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const vpc = new ec2.Vpc(this, 'NewVpc', {
+      maxAzs: 3
     });
 
-    const topic = new sns.Topic(this, 'HelloCdkTopic');
+    const cluster = new ecs.Cluster(this, 'newCluster', { vpc });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      'newFargate',
+      {
+        cluster,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample')
+        }
+      }
+    );
+
+    new cdk.CfnOutput(this, 'newLoadBalancer', {
+      value: fargateService.loadBalancer.loadBalancerDnsName
+    });
   }
 }
--- test/hello-cdk.test.ts
+++ test/hello-cdk.test.ts
@@ -2,20 +2,28 @@ import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import HelloCdk = require('../lib/hello-cdk-stack');
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
-    }));
-});
-
-test('SNS Topic Created', () => {
+test('VPC Created', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
   // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource('AWS::EC2::VPC'));
+});
+
+test('ECS cluster Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResource('AWS::ECS::Cluster'));
+});
+
+test('Load Balancer Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    haveResource('AWS::ElasticLoadBalancingV2::LoadBalancer')
+  );
 });
