5426ac8e1941cb3ffddf3c7010d4b91784abfa13
Bryan Chua <me@bryanchua.com>
2019-11-18 14:27:51 +0800

    update test

--- test/hello-cdk.test.ts
+++ test/hello-cdk.test.ts
@@ -2,36 +2,15 @@ import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import HelloCdk = require('../lib/hello-cdk-stack');
 
-test('VPC Created', () => {
+test('A Sample App', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
+  const stack = new HelloCdk.HelloCdkStack(app, 'MySampleApp');
   // THEN
   expectCDK(stack).to(haveResource('AWS::EC2::VPC'));
-});
-
-test('ECS cluster Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
-  // THEN
   expectCDK(stack).to(haveResource('AWS::ECS::Cluster'));
-});
-
-test('Load Balancer Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
-  // THEN
   expectCDK(stack).to(
     haveResource('AWS::ElasticLoadBalancingV2::LoadBalancer')
   );
-});
-
-test('CloudWatch Dashboard Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
-  // THEN
   expectCDK(stack).to(haveResource('AWS::CloudWatch::Dashboard'));
 });
